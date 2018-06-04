8b71995c5acec0c7acebeaad810fc45a802e5118
Yeongho Kim <yeonghoey@gmail.com>
2018-06-05 00:29:28 +0900

    Support half flag for img paste

--- yhy/commands/img/paste.py
+++ yhy/commands/img/paste.py
@@ -3,12 +3,15 @@ import os
 import click
 
 from yhy import require
-from yhy.core.img import clipboard_img, save
+from yhy.core.img import clipboard_img, hexhash, save
 
 
 @click.option('-C', '--directory', type=click.Path(exists=False))
-def command(directory):
-    img, hash_ = clipboard_img()
+@click.option('--half', is_flag=True)
+def command(directory, half):
+    img = clipboard_img(half=half)
+
+    hash_ = hexhash(img)
     filename = f'{hash_}.png'
 
     if not directory:
--- yhy/core/img/__init__.py
+++ yhy/core/img/__init__.py
@@ -4,15 +4,27 @@ import click
 from PIL import ImageGrab
 
 
-def clipboard_img():
+def clipboard_img(half=False):
     img = ImageGrab.grabclipboard()
+
     if img is None:
         raise click.UsageError('Clipboard does not contain image data')
-    else:
-        h = sha1()
-        h.update(img.tobytes())
-        return (img, h.hexdigest())
 
+    if half:
+        img = scaled(img, 0.5)
+
+    return img
+
+
+def hexhash(img):
+    ho = sha1()
+    ho.update(img.tobytes())
+    return ho.hexdigest()
 
 def save(img, path):
     img.save(path)
+
+
+def scaled(img, s):
+    w, h = img.size
+    return img.resize((int(w*s), int(h*s)))
