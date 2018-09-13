6e02085f9bb741f804dcc65983d7759bf1a19c84
Yeongho Kim <yeonghoey@gmail.com>
2018-09-14 06:40:45 +0900

    Add clip

---
+++ yhy/commands/clip.py
@@ -0,0 +1,11 @@
+import sys
+
+import click
+import pyperclip
+
+
+def command():
+    text = '\n'.join(l for l in sys.stdin)
+    text = text.strip()
+    pyperclip.copy(text)
+    click.echo(text)
